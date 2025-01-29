1a2d78b794d0d4e47de92b0d891fc9f675f51d1d
irfan <irfanburak2000@hotmail.com>
2025-01-29 00:02:53 -0500

    added pages

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,20 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "Hello World!"
 
+@app.route('/welcome')
+def welcome():
+    return "Welcome to the world of Flask!"
 
-
+@app.route('/welcome/<name>')
+def welcome_name(name):
+    return f"Welcome to the world of Flask, {name}!"
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
+    app.run(debug=True, port=5000)
     # app.run(host= '0.0.0.0', port=8081)
--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -2,13 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return render_template("index.html", number1=50, number2=20)
 
+@app.route("/<string:num1>/<string:num2>")
+def number(num1, num2):
+    return render_template("index.html", number1=int(num1), number2=int(num2))
 
-
-
-
+@app.route("/multiply/<string:num1>/<string:num2>")
+def multiply(num1, num2):
+    if num1.isdigit() and num2.isdigit():
+        return render_template("body.html", value1=int(num1), value2=int(num2), sum=int(num1)*int(num2))
+    else:
+        return "Please enter valid number"
 
 
 if __name__== "__main__":
-    app.run(debug=True, port=30000)
+    app.run(debug=True, port=3000)
     # app.run(host= '0.0.0.0', port=8081)
